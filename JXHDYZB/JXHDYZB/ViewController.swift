4a7f00a653c04df14a020d48f6d56b8f711b836e
juxiaohui <123456>
2016-11-08 10:15:07 +0800

    创建项目

---
+++ JXHDYZB/JXHDYZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JXHDYZB
+//
+//  Created by juxiaohui on 16/11/8.
+//  Copyright © 2016年 juxiaohui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
